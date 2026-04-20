a2c72361cdc3c5d4082a59c8947ae60e9ed3929d
Lord Fiifi Quayle <lordfiifiquayle@Lords-MacBook.local>
2026-04-20 20:31:43 +0000

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>Welcome to the About page.</p>
+    </main>
+  )
+}
---
+++ app/insights/page.tsx
@@ -0,0 +1,8 @@
+export default function Insights() {
+  return (
+    <main>
+      <h1>Insights</h1>
+      <p>Welcome to the Insights page.</p>
+    </main>
+  )
+}
